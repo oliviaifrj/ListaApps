be8927f029b32c98f873843122f02d85f7251764
xXJonasGamesXx <olivia.ifrj@gmail.com>
2025-11-11 10:49:15 -0300

    *-a)

---
+++ exercicio8-a).jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function AdicionarLista(){
+  const [item, setItem] = useState("");
+  const [lista, setLista] = useState([]);
+  const adicionarItem = () => {
+    if (item.trim()) {
+      setLista([...lista, item]);
+      setItem('');
+    }
+  }
+
+  return (
+    <div>
+      <input
+      type='text'
+      value={item}
+      onChange={(e) => setItem(e.target.value)}
+      placeholder='Digite um item'
+      />
+      <button onClick={adicionarItem}>Adicionar</button>
+      <ul>
+        {lista.map((itemLista, index) => (
+          <li key={index}>{itemLista}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+
+export default AdicionarLista
+
+
+
+
