d9c9e2b4c680b6a865b907f9f509364792f22c80
xXJonasGamesXx <olivia.ifrj@gmail.com>
2025-11-11 10:35:37 -0300

    7-a)

---
+++ exercicio7-a).jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function InputTempoReal(){
+  const [texto, setTexto] = useState('');
+
+  return (
+    <div>
+      <input
+      type="text"
+      value={texto}
+      onChange={(e) => setTexto(e.target.value)}
+      placeholder= "Digite algo..."
+      />
+      <p>Você digitou: {texto}</p>
+    </div>
+  );
+}
+
+export default InputTempoReal;
+
+
