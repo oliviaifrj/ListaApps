558c980930435e48ce1b191abb535ee9c568c4dd
xXJonasGamesXx <olivia.ifrj@gmail.com>
2025-11-11 09:00:51 -0300

    1-a)

---
+++ exercicio1-a).jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function OlaMundo(){
+  return <h1>Bem Vindo ao React</h1>
+}
+
+
+export default OlaMundo
+
