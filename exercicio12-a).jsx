177b46b4df5cd94d0cacd61800639bbda2b2d7b5
xXJonasGamesXx <olivia.ifrj@gmail.com>
2025-11-18 09:28:57 -0300

    12-a)

---
+++ exercicio12-a).jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function BotaoCurtir() {
+  const[likes, setLikes] = useState(0);
+  const [curtido, setCurtido] = useState(false);
+
+  const toggleLike = () => {
+    if (curtido) {
+      setLikes(likes - 1);
+      setCurtido(false);
+    } else{
+      setLikes(likes + 1);
+      setCurtido(true);
+    }
+  };
+
+  return (
+    <div>
+      <button
+      onClick={toggleLike}
+      style={{
+        backgroundColor: curtido ? "#2b63e7ff" : "#ffff",
+        color: curtido ? 'white' : 'black',
+        border: 'none',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        cursor: 'pointer'
+      }}>
+        👍 {curtido ? 'Curtido ' : 'Curtir '}({likes})
+        </button>
+    </div>
+  );
+}
+
+export default BotaoCurtir
+
+
+
